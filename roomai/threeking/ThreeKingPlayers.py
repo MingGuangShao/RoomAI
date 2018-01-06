530f3cd28ca6bd778f109ee1143f861a8696f179
unknown <690714362@qq.com>
2018-01-06 14:16:20 +0800

    players

---
+++ roomai/threeking/ThreeKingPlayers.py
@@ -0,0 +1,25 @@
+#!/bin/python
+
+import roomai.threeking
+from roomai.threeking import ThreekingAction
+from roomai.threeking import ThreekingPokerCard
+
+
+class Player(roomai.common.AbstractPlayer):
+    '''
+    '''
+    def take_action(self):
+        '''
+        define a policy to play the game
+        return an action object
+        ''' 
+    def receive_info(self, info):
+        '''
+        '''
+        self.public_state = info.public_state
+        self.avaliable_actions = info.person_state.avaliable_actions
+
+    def reset(self):
+        '''
+        '''
+        pass
