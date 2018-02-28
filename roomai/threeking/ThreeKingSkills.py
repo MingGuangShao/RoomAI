8615aa616032fcffa0828b9871efb8e7c3858c9f
unknown <690714362@qq.com>
2018-02-28 23:18:55 +0800

    unit test

--- roomai/threeking/ThreeKingSkills.py
+++ roomai/threeking/ThreeKingSkills.py
@@ -10,22 +10,33 @@ def take_action(pu,pr,pes,action):
     target_zones    = action.targert_zones
     target_cards    = action.target_cards 
     
-    if skill_name in []:
+    if skill_name in ["Pass"]:
         globals().get_str(skill_name)(pu,pr,pes)
 
-    elif skill_name in []:
+    elif skill_name in ["Get","Equip","Fate","NanManRuQin","WuZhongShengYou","WanJianQiFa","TaoYuanJieYi"]:
         globals().get_str(skill_name)(pu,pr,pes,card)
 
-    elif skill_name in []:
+    elif skill_name in ["Sha","FengTianHuaJi","JueDou","WuXieKeJi","LeBuSiShu"]:
         globals().get_str(skill_name)(pu,pr,pes,card,targets)
 
-    elif skill_name in []:
+    elif skill_name in ["ShunShouQianYang","GuoHeChaiQiao"]:
         globals().get_str(skill_name)(pu,pr,pes,card,targets,other_targets)
     
-    elif skill_name in []:
+    elif skill_name in ["JieDaoShaRen"]:
         globals().get_str(skill_name)(pu,pr,pes,card,targets,targets_zones,target_cards)
 
-def NANMAN(pu,pr,pes):
+
+def Sha(pu, pr, pes, card, targets):
+    '''
+    '''
+    pes[pu.turn].__del_cards__(card)
+    pu.__num_hand_cards__[turn] = len(pes[pu.turn].hand_cards)
+
+    pu.__add_discard_cards__(card)
+    pu.__num_discard_cards__    = len(pu.num_discard_cards) 
+
+    
+def NanManRuQin(pu,pr,pes):
     '''
     '''
     pes[turn].__del__cards__(card)
@@ -38,11 +49,11 @@ def NANMAN(pu,pr,pes):
 
     return pu,pr,pes
 
-def WZSY(pu,pr,pes):
+def WuZhongShengYou(pu,pr,pes):
     '''
     '''
 
-def SSQY(pu,pr,pes):
+def ShunShouQianYang(pu,pr,pes):
     '''
     '''
     
