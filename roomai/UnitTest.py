7b10f45c77a9c2de4ba3c4c555f5bd0d70581982
unknown <690714362@qq.com>
2018-03-03 16:46:43 +0800

    unit test

--- roomai/UnitTest.py
+++ roomai/UnitTest.py
@@ -2,21 +2,15 @@ import sys
 sys.path.append("..")
 
 from roomai.threeking import *;
+from roomai.threeking.ThreeKingPlayers import *;
 import random
+
 
 if __name__ == "__main__":
 
+    players = [Player() for i in range(8)]
     env = ThreeKingEnv()
-    params = dict()
-    env.init(params)
-    print "instance enc: ", env
-    #print all method and f in env
-    for i in dir(env):
-        print i
+    ThreeKingEnv.compete(env, players)
 
-    print "params in env: ", env.__params__.keys()
-    print "players info in env: ", env.__params__["players_info"]
-    print "num players in env: ", env.__params__["num_players"]
-    print "lord id in public_state: ", env.public_state.__lord_id__
-    print "num of hand cards in public_state: ",env.public_state.__num_hand_cards__
+
 
