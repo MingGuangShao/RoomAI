fadce89728a2ed8a959728fbecd5fc5021b6f28b
unknown <690714362@qq.com>
2018-05-14 06:23:39 +0800

    add change

--- roomai/threeking/ThreeKingPlayers.py
+++ roomai/threeking/ThreeKingPlayers.py
@@ -8,21 +8,24 @@ from roomai.threeking import ThreeKingPokerCard
 class Player(roomai.common.AbstractPlayer):
     '''
     '''
+
     def take_action(self):
         '''
         define a policy to play the game
         return an action object
         '''
         #implement your code here
-        #a = strategy(self.avaliable_actions.values())
+        from random import choice
+        a = choice(self.available_actions.values())
         # For Unit Test
-        a = "Sha,SHA_7_spade_0,MaChao" 
+        #a = "Sha,SHA_7_spade_0,MaChao" 
         return ThreeKingAction.lookup(a)
 
     def receive_info(self, info):
         '''
         '''
         #question? why self.public_state
+        #print "received!"
         self.public_state = info.public_state
         self.available_actions = info.person_state.available_actions
 
